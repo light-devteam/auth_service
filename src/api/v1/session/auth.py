747d2ac42e96c962afad15dd6e79c9ec63fc8b37
TiRe <zientenin@mail.ru>
2025-11-05 15:41:05 +0300

    feat: move token validation into dependency

--- src/api/v1/session/auth.py
+++ src/api/v1/session/auth.py
@@ -1,30 +1,9 @@
-from fastapi import Request, Response, Security
-from fastapi.security import APIKeyCookie, HTTPAuthorizationCredentials, HTTPBearer
+from fastapi import Depends
 
 from src.api.v1.session.router import router
-from src.services import SessionsService
-from src.exceptions import AccessTokenInvalid
-from src.enums import TokenTypes
+from src.dependencies import get_validated_token
 
-
-cookie_security = APIKeyCookie(name='access_token', auto_error=False)
-bearer_security = HTTPBearer(auto_error=False)
 
 @router.get('/auth')
-async def auth(
-    request: Request,
-    response: Response,
-    cookie_token: str | None = Security(cookie_security),
-    bearer_token: HTTPAuthorizationCredentials | None = Security(bearer_security),
-) -> None:
-    token = None
-    token_type = None
-    if cookie_token:
-        token = cookie_token
-        token_type = TokenTypes.BEARER.value
-    elif bearer_token:
-        token = bearer_token.credentials
-        token_type = bearer_token.scheme
-    else:
-        raise AccessTokenInvalid()
-    await SessionsService.validate_access_token(token_type, token)
+async def auth(token: str = Depends(get_validated_token)) -> None:
+    return
---
+++ src/dependencies/__init__.py
@@ -0,0 +1,6 @@
+from src.dependencies.get_validated_token import get_validated_token
+
+
+__all__ = [
+    'get_validated_token',
+]
