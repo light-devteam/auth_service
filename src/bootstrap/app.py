9901083a44eb3a53aeac3a0911eccbd6e2de439a
TiRe <zientenin@mail.ru>
2025-12-06 05:01:56 +0300

    fix: exceptions domain

--- src/bootstrap/app.py
+++ src/bootstrap/app.py
@@ -1,16 +1,23 @@
 from fastapi import FastAPI
+from dependency_injector.wiring import inject, Provide
 
 from src.bootstrap.lifespan import LifespanManager
 # from src.bootstrap.middleware import register_middlewares
 from src.bootstrap.exception_handler import ExceptionHandler
+from src.shared.infrastructure.logger import LoggerFactory
 
 class App:
     def __init__(self) -> None:
         self.lifespan_manager = LifespanManager()
         self.__app = None
         self.__logger = None
+        self._routers = []
 
-    def create(self) -> FastAPI:
+    @inject
+    def create(
+        self,
+        logger_factory: LoggerFactory = Provide['logger_factory'],
+    ) -> FastAPI:
         self.__app = FastAPI(
             title='Auth Service',
             version='1.0.0',
@@ -18,7 +25,7 @@ class App:
             redoc_url='/redoc',
             lifespan=self.lifespan_manager.lifespan,
         )
-        self.__logger = self.lifespan_manager.container.logger_factory().get_logger(__name__)
+        self.__logger = logger_factory.get_logger(__name__)
         # register_middlewares(app)
         ExceptionHandler(self.__app)
         self.__include_routers()
@@ -26,7 +33,6 @@ class App:
         return self.__app
 
     def __include_routers(self) -> None:
-        # app.include_router(well_known_router)
-        # app.include_router(api_router)
-        self.__logger.debug('All routers included')
-        ...
+        for router in self._routers:
+            self.__app.include_router(router)
+        self.__logger.debug(f'Included {len(self._routers)} routers')
