0857466768f9fa95e3b89ec4e71d2fd5f6ec39a8
TiRe <zientenin@mail.ru>
2026-01-12 21:49:34 +0300

    fix: endpoint name

--- src/contexts/authentication/delivery/http/accounts/__init__.py
+++ src/contexts/authentication/delivery/http/accounts/__init__.py
@@ -2,7 +2,7 @@ from src.contexts.authentication.delivery.http.accounts.router import router
 from src.contexts.authentication.delivery.http.accounts.create import router
 from src.contexts.authentication.delivery.http.accounts.get import router
 from src.contexts.authentication.delivery.http.accounts.get_identities import router
-from src.contexts.authentication.delivery.http.accounts.get_identity_by_provider_id import router
+from src.contexts.authentication.delivery.http.accounts.get_identity_provider import router
 from src.contexts.authentication.delivery.http.accounts.authenticate import router
 
 
